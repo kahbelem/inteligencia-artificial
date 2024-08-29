8923b18bec5c683451caa4d192201bd584bd326c
Kássia Belém Anacleto <114186521+kahbelem@users.noreply.github.com>
2024-08-29 11:06:55 -0300

    .

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 const caixaPrincipal = document.querySelector(".caixa-principal");
-const caixaPerguntas = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-reultado");
+const caixaResultado = document.querySelector(".caixa-resultado");
+
+const perguntas = [
+   {
+    enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que "
+   }
